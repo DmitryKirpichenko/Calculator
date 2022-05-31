2145d506a81fe3bbe538ce30fa9016ca63c67793
DmitryKirpichenko <mr.dima.kirpichenko@gmail.com>
2022-05-31 17:37:20 +0300

    Add redux-presist for save history

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App/index.jsx';
-import store from './redux/store.js';
+import store, {persistor}from './redux/store.js';
 import { Provider } from 'react-redux';
 import { GlobalStyle } from './components.js';
+import {PersistGate} from 'redux-persist/integration/react'
 
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>
 )
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import expressionSlice from "./expressionSlice";
 import hisrorySlice from "./hisrorySlice";
 import themeSlice from "./themeSlice";
 
-export default configureStore({
-    reducer: {
-        expression: expressionSlice,
-        history: hisrorySlice,
-        theme: themeSlice,
-    },
+import { persistStore, 
+         persistReducer,
+         FLUSH,
+         REHYDRATE,
+         PAUSE,
+         PERSIST,
+         PURGE,
+         REGISTER, } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+    expression: expressionSlice,
+    history: hisrorySlice,
+    theme: themeSlice,
 })
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+ 
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export default store
+export const persistor = persistStore(store)
